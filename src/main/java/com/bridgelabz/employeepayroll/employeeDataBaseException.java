0cdb24f8a3595a629fa902995b97db5ed0de153d
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-04 02:22:23 +0530

    Select Database from Programme And throw Custom Exception.

---
+++ src/main/java/com/bridgelabz/employeepayroll/employeeDataBaseException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayroll;
+
+public class employeeDataBaseException extends Exception {
+    public employeeDataBaseException(String message)
+    {
+        super(message);
+    }
+}
